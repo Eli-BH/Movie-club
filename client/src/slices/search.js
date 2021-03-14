761e7a4dc850467b2a9c82efc8fad211851e9339
Eli-BH <eli.i.henderson95@gmail.com>
2021-03-14 14:46:28 -0400

    SETUP: search results redux

--- client/src/slices/search.js
+++ client/src/slices/search.js
@@ -1,7 +1,59 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//A slice for a single Movie Pages
 
 const initialState = {
   loading: false,
   hasError: false,
   searchResults: [],
 };
+
+const searchResultsSlice = createSlice({
+  name: "singleMovie",
+  initialState,
+  reducers: {
+    getSearchResults: (state) => {
+      state.loading = true;
+    },
+    getSearchResultsSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.hasError = false;
+      state.searchResults = payload;
+    },
+    getSearchFailure: (state) => {
+      state.loading = false;
+      state.hasError = true;
+    },
+  },
+});
+
+//actions from slice
+export const {
+  getSearchResults,
+  getSearchResultsSuccess,
+  getSearchResultsFailure,
+} = searchResultsSlice.actions;
+
+//A selector
+export const searchResultsSelector = (state) => state.searchResults;
+
+//reducer
+export default searchResultsSlice.reducer;
+
+//Thunk
+export function fetchSearchResults(query) {
+  return async (dispatch) => {
+    dispatch(getSearchResults());
+    try {
+      query = query.replacAll(" ", "%20");
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=0ca4f16446cc1bca4c690abae99b5e52&language=en-US&query=${query}&include_adult=false`;
+      const response = await axios.get(url);
+      const { data } = response;
+      dispatch(getSearchResultsSuccess(data));
+    } catch (error) {
+      dispatch(getSearchResultsFailure());
+      console.log(error);
+    }
+  };
+}
