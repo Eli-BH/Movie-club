aad2eda913b2247727e5f6ada709fac5f289a4cf
Eli-b-good <eli.i.henderson95@gmail.com>
2021-03-13 23:53:27 -0500

    ADDITION: grid styling for movie pages

--- client/src/Pages/MoviePage.js
+++ client/src/Pages/MoviePage.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { singleMovieSelector, fetchSingleMovie } from "../slices/singleMovie";
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 import axios from "axios";
 
 const MoviePage = ({ match }) => {
@@ -26,6 +30,42 @@ const MoviePage = ({ match }) => {
   }, [dispatch, id]);
   console.log(watchProviders);
   console.log(singleMovie);
+  const handleRedirect = (local) => {
+    switch (local) {
+      case "Apple iTunes":
+        window.location.href = " https://www.apple.com/itunes/";
+        break;
+      case "Netflix":
+        window.location.href = "https://netflix.com";
+        break;
+      case "Google Play Movies":
+        window.location.href =
+          "https://play.google.com/store/movies?hl=en_US&gl=US";
+        break;
+      case "Amazon Video":
+        window.location.href =
+          "https://www.amazon.com/Prime-Video/b?node=2676882011";
+        break;
+      case "YouTube":
+        window.location.href = "youtube.com";
+        break;
+      case "Vudu":
+        window.location.href = "https://www.vudu.com/";
+        break;
+      case "Microsoft Store":
+        window.location.href = "https://www.microsoft.com/en-us/movies-and-tv";
+        break;
+      case "Redbox":
+        window.location.href = "redbox.com";
+        break;
+      case "DIRECTV":
+        window.location.href = "https://www.directv.com/movies";
+        break;
+      default:
+        return;
+    }
+  };
+
   return singleMovie ? (
     <div>
       {" "}
@@ -99,14 +139,7 @@ const MoviePage = ({ match }) => {
               <p>
                 <i>{singleMovie.tagline}</i>
               </p>
-              {watchProviders ? (
-                <div className="movie-banner-watch-providers">
-                  Watch rent or buy from these providers
-                  {watchProviders.buy && <div>buy</div>}
-                  {watchProviders.flatrate && <div>stream</div>}
-                  {watchProviders.rent && <div>rent</div>}
-                </div>
-              ) : null}
+
               <h3>Overview</h3>
               <p>{singleMovie.overview}</p>
             </div>
@@ -130,36 +163,241 @@ const MoviePage = ({ match }) => {
           </div>
         </div>
       }
-      <div className="container-xl">
-        <div className="movie-page-left mt-5 mb-2">
-          <div>
-            <h2>Top Billed Cast</h2>
-            <div className="movie-page-cast-slider mt-4" id="mp-cast-slider">
-              {singleMovie.credits.cast
-                .filter((item) => item.profile_path)
-                .slice(0, 20)
-                .map((item) => {
-                  return (
-                    <div key={item.name} className="movie-page-cast-img">
-                      <img
-                        src={`https://www.themoviedb.org/t/p/original${item.profile_path}`}
-                        alt={item.name}
-                      />
-                      <p>
-                        <b>{item.name}</b>
-                      </p>
+      <div className=" movie-page-container">
+        <Row className=" movie-page-divider">
+          <Col lg={9} className="movie-page-left mt-5 mb-2">
+            <div>
+              <h2>Top Billed Cast</h2>
+              <div className="movie-page-cast-slider mt-4" id="mp-cast-slider">
+                {singleMovie.credits.cast
+                  .filter((item) => item.profile_path)
+                  .slice(0, 20)
+                  .map((item) => {
+                    return (
+                      <div key={item.name} className="movie-page-cast-img">
+                        <img
+                          src={`https://www.themoviedb.org/t/p/original${item.profile_path}`}
+                          alt={item.name}
+                        />
+                        <p>
+                          <b>{item.name}</b>
+                        </p>
 
-                      <p>{item.character}</p>
-                    </div>
-                  );
-                })}
+                        <p>{item.character}</p>
+                      </div>
+                    );
+                  })}
+              </div>
             </div>
-          </div>
-        </div>
 
-        <div className="movie-page-right">
-          <div></div>
-        </div>
+            <div>
+              <h2>Top Billed Crew</h2>
+              <div className="movie-page-cast-slider mt-4" id="mp-cast-slider">
+                {singleMovie.credits.crew
+                  .filter((item) => item.profile_path)
+                  .slice(0, 30)
+                  .map((item, index) => {
+                    return (
+                      <div key={index} className="movie-page-cast-img">
+                        <img
+                          src={`https://www.themoviedb.org/t/p/original${item.profile_path}`}
+                          alt={item.name}
+                        />
+                        <p>
+                          <b>{item.name}</b>
+                        </p>
+
+                        <p>{item.department}</p>
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+
+            <div className="movie-page-media my-5">
+              {
+                <div>
+                  <h2>Media</h2>
+                  <Tabs defaultActiveKey="videos">
+                    <Tab eventKey="videos" title="Videos">
+                      <div className="container m-3 media-videos">
+                        {singleMovie.videos ? (
+                          singleMovie.videos.results.map((item) => {
+                            return (
+                              <div key={item.id} className="media-video-item">
+                                <iframe
+                                  title={item.name}
+                                  width="360"
+                                  height="215"
+                                  src={`https://www.youtube.com/embed/${item.key}`}
+                                  frameborder="0"
+                                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                  allowfullscreen
+                                ></iframe>
+                              </div>
+                            );
+                          })
+                        ) : (
+                          <p>No videos</p>
+                        )}
+                      </div>
+                    </Tab>
+                    <Tab eventKey="similar" title="Similar Movies">
+                      <div className="container m-3 media-similar">
+                        {singleMovie.videos ? (
+                          singleMovie.similar.results.map((item) => {
+                            return (
+                              <div className="similar-img-div">
+                                <Link to={`/movie/${item.id}`}>
+                                  <img
+                                    className="similar-img"
+                                    src={`https://www.themoviedb.org/t/p/original${item.poster_path}`}
+                                    alt={item.id}
+                                  />
+                                  <p>{item.title}</p>
+                                </Link>
+                              </div>
+                            );
+                          })
+                        ) : (
+                          <h3>No similar movies found</h3>
+                        )}
+                      </div>
+                    </Tab>
+                  </Tabs>
+                </div>
+              }
+            </div>
+
+            {singleMovie.recommendations && (
+              <div>
+                <h1>Recommendations</h1>
+                <div className="recommendations">
+                  {singleMovie.recommendations.results.map((item) => {
+                    return (
+                      <div key={item.id} className="recommendations-container">
+                        <Link to={`/movie/${item.id}`}>
+                          <img
+                            className="recommendations-img"
+                            src={`http://www.themoviedb.org/t/p/original${item.poster_path}`}
+                            alt={item.title}
+                          />
+                          <p>{`${item.title}`}</p>
+                        </Link>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </Col>
+
+          <Col lg={3} className="movie-page-right  mt-5 mb-2">
+            <div className="movie-stats">
+              <div id="status" className="movie-stats-item">
+                <h1>Status</h1>
+                <p>{singleMovie.status}</p>
+              </div>
+              <div id="language" className="movie-stats-item">
+                <h1>Spoken languages</h1>
+                {singleMovie.spoken_languages.map((item) => {
+                  return <p>{item.english_name}</p>;
+                })}
+              </div>
+              <div id="budget" className="movie-stats-item">
+                <h1>Budget</h1>
+                <p>
+                  {new Intl.NumberFormat("en-IN", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(singleMovie.budget) || null}
+                </p>
+              </div>
+              <div id="revenue" className="movie-stats-item">
+                <h1>Revenue</h1>
+                <p>
+                  {new Intl.NumberFormat("en-IN", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(singleMovie.revenue) || null}
+                </p>
+              </div>
+              <div id="production-company" className="movie-stats-item">
+                <h1>Production companies</h1>
+
+                {singleMovie.production_companies.map((item) => {
+                  return <p>{item.name}</p>;
+                })}
+              </div>
+              <div id="keywords" className="movie-stats-item">
+                <h1>Keywords</h1>
+
+                {singleMovie.production_companies.map((item) => {
+                  return <p>{item.name}</p>;
+                })}
+              </div>
+
+              {watchProviders ? (
+                <div className="watchProviders">
+                  {watchProviders.flatrate && (
+                    <div className="watchProviders-watch">
+                      <h3>Stream this movie </h3>
+                      {watchProviders.flatrate.map((item) => {
+                        return item ? (
+                          <img
+                            src={`https://www.themoviedb.org/t/p/original${item.logo_path}`}
+                            alt={item.provider_name}
+                            onClick={() => {
+                              handleRedirect(item.provider_name);
+                            }}
+                          />
+                        ) : (
+                          <p>loading</p>
+                        );
+                      })}
+                      {watchProviders.rent && (
+                        <div className="watchProviders-watch">
+                          <h3>Rent this movie </h3>
+                          {watchProviders.rent.map((item) => {
+                            return item ? (
+                              <img
+                                src={`https://www.themoviedb.org/t/p/original${item.logo_path}`}
+                                alt={item.provider_name}
+                                onClick={() => {
+                                  handleRedirect(item.provider_name);
+                                }}
+                              />
+                            ) : (
+                              <h3>loading</h3>
+                            );
+                          })}
+                          {watchProviders.buy && (
+                            <div className="watchProviders-watch">
+                              <h3>Buy this movie </h3>
+                              {watchProviders.buy.map((item) => {
+                                return item ? (
+                                  <img
+                                    src={`https://www.themoviedb.org/t/p/original${item.logo_path}`}
+                                    alt={item.provider_name}
+                                    onClick={() => {
+                                      handleRedirect(item.provider_name);
+                                    }}
+                                  />
+                                ) : (
+                                  <h3>Loading</h3>
+                                );
+                              })}
+                            </div>
+                          )}
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </div>
+              ) : null}
+            </div>
+          </Col>
+        </Row>
       </div>
     </div>
   ) : (
